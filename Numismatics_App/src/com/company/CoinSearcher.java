242fb47dec64b8f5332447af2a8bc36b607380b2
InsaneD69 <90348162+InsaneD69@users.noreply.github.com>
2022-03-18 15:32:05 +0300

    Numismatics_App
    основа класса Coin, добавлено поле с датой создания монеты (будет использоваться потом для сравнения цен в разные даты, возможно создание наглядных графиков)
    функция getCountries() по добавлению списка всех стран каждый раз после запуска кода
    цена монеты типа Optional<Float>, для корректной  работы при отсутствии цены на монету на сайте (такое может быть)
     основа для функции  smartCoutrySelection, которая будет (пока что) будет сравнивать вводимую страну с тем списком стран, который уже был загружен с помощью (нужно будет изменить на посимвольное считывание сканером)

---
+++ Numismatics_App/src/com/company/CoinSearcher.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class CoinSearcher {
+
+
+    private Document doc;
+
+
+    private Elements countries; //список со всеми странами
+
+
+    public CoinSearcher() throws IOException {  //отвечает за подгрузку нужной инфы дляотпимизации поиска
+
+        getCountries();
+
+    }
+
+
+
+
+    public void getCountries() throws IOException {
+
+        doc = Jsoup.connect("https://en.ucoin.net/catalog").get();
+        countries = doc.getElementsByAttributeValue("class","left wrap nopad");
+        if(countries==null){System.out.println("Error parse countries"); };
+
+        testCountries(countries);
+
+    }
+
+
+    public static void smartCoutrySelection(String country){
+        System.out.println("im working");
+
+    }
+
+    public void testCountries(Elements countries) {
+
+        System.out.println(countries.size());
+
+        countries.forEach(scope->{
+
+            String rwer=scope.text();
+
+            System.out.print(rwer+", ");
+
+
+        });
+        System.out.println();
+
+    }
+
+
+}
